f177653783939557cf28dd5e48f834e927dc9c64
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2024-02-14 23:52:23 +0100

    feat: modularise pages

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,17 +3,5 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import LeaderBoard from "./pages/LeaderBoard";
-import Contribute from "./components/Contribute";
-import AddFood from "./pages/AddFood";
-import Editor from "./pages/Editor";
 
-export {
-  Login,
-  NotFound,
-  CreateAccount,
-  Dashboard,
-  LeaderBoard,
-  Contribute,
-  AddFood,
-  Editor,
-};
+export { Login, NotFound, CreateAccount, Dashboard, LeaderBoard };
